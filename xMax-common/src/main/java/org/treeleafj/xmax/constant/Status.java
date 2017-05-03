c5bb9ae9fcb157d9235000d79eae8158562136bb
treeleafj <treeleafj@outlook.com>
2017-05-03 15:13:07 +0800

    添加状态常量,用于其它model继承

---
+++ xMax-common/src/main/java/org/treeleafj/xmax/constant/Status.java
@@ -0,0 +1,19 @@
+package org.treeleafj.xmax.constant;
+
+/**
+ * 状态
+ * <p>
+ * Created by leaf on 2017/5/3.
+ */
+public interface Status {
+
+    /**
+     * 状态-启用
+     */
+    Integer STATUS_ENABLE = 1;
+
+    /**
+     * 状态-禁用
+     */
+    Integer STATUS_DISABLE = 0;
+}
