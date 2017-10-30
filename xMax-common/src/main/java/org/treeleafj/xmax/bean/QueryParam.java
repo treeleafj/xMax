ecbb928a0d072610c33907e821338301ca4f0254
treeleafj <treeleafj@outlook.com>
2017-10-30 15:23:39 +0800

    添加页面查询父类

---
+++ xMax-common/src/main/java/org/treeleafj/xmax/bean/QueryParam.java
@@ -0,0 +1,11 @@
+package org.treeleafj.xmax.bean;
+
+import java.io.Serializable;
+
+/**
+ * 数据查询父类,代表了页面请求参数
+ * <p>
+ * Created by leaf on 2017/10/30.
+ */
+public abstract class QueryParam implements Serializable {
+}
